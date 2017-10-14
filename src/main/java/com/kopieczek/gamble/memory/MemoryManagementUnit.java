c13c6d40cba524be7e9baf30fea5bf175b9f3238
Stefan Kopieczek <stefankopieczek@gmail.com>
2017-10-14 17:20:45 +0100

    MMU: Add ability to read from all memory addresses
    ... except ROM 0 because I haven't implemented the BIOS switch yet.

--- src/main/java/com/kopieczek/gamble/memory/MemoryManagementUnit.java
+++ src/main/java/com/kopieczek/gamble/memory/MemoryManagementUnit.java
@@ -1,6 +1,28 @@
 package com.kopieczek.gamble.memory;
 
 public class MemoryManagementUnit {
+    public static final int BIOS_START       = 0x0000;
+    public static final int BIOS_SIZE        = 0x0100;
+    public static final int ROM_0_START      = 0x0000;
+    public static final int ROM_0_SIZE       = 0x4000;
+    public static final int ROM_1_START      = 0x4000;
+    public static final int ROM_1_SIZE       = 0x4000;
+    public static final int VRAM_START       = 0x8000;
+    public static final int VRAM_SIZE        = 0x2000;
+    public static final int EXT_RAM_START    = 0xa000;
+    public static final int EXT_RAM_SIZE     = 0x2000;
+    public static final int RAM_START        = 0xc000;
+    public static final int RAM_SIZE         = 0x2000;
+    public static final int SHADOW_RAM_START = 0xe000;
+    public static final int SPRITES_START    = 0xfe00;
+    public static final int SPRITES_SIZE     = 0x00a0;
+    public static final int DEAD_AREA_START  = 0xfea0;
+    // 0x60 bytes of unusable memory between sprites and IO.
+    public static final int IO_AREA_START    = 0xff00;
+    public static final int IO_AREA_SIZE     = 0x0080;
+    public static final int ZRAM_START       = 0xff80;
+    public static final int ZRAM_SIZE        = 0x0080;
+
     private final MemoryModule bios;
     private final MemoryModule rom0;
     private final MemoryModule rom1;
@@ -33,15 +55,15 @@ public class MemoryManagementUnit {
     }
 
     private void validateMemoryModuleSizes() throws IllegalArgumentException {
-        assertModuleSize("BIOS", bios,0x100);
-        assertModuleSize("ROM0", rom0,0x4000);
-        assertModuleSize("ROM1", rom1,0x4000);
-        assertModuleSize("GPU VRAM", gpuVram,0x2000);
-        assertModuleSize("External RAM", extRam,0x2000);
-        assertModuleSize("Working RAM", ram,0x2000);
-        assertModuleSize("Sprite Space", sprites,0xa0);
-        assertModuleSize("Memory-mapped IO", io,0x80);
-        assertModuleSize("High-page RAM", zram,0x80);
+        assertModuleSize("BIOS", bios, BIOS_SIZE);
+        assertModuleSize("ROM0", rom0, ROM_0_SIZE);
+        assertModuleSize("ROM1", rom1, ROM_1_SIZE);
+        assertModuleSize("GPU VRAM", gpuVram, VRAM_SIZE);
+        assertModuleSize("External RAM", extRam, EXT_RAM_SIZE);
+        assertModuleSize("Working RAM", ram, RAM_SIZE);
+        assertModuleSize("Sprite Space", sprites, SPRITES_SIZE);
+        assertModuleSize("Memory-Mapped IO", io, IO_AREA_SIZE);
+        assertModuleSize("High-Page RAM", zram, ZRAM_SIZE);
     }
 
     private static void assertModuleSize(String moduleName, MemoryModule module, int expectedSize)
@@ -58,10 +80,29 @@ public class MemoryManagementUnit {
     }
 
     public int readByte(int address) {
-        if (address < 0x4000) {
-            return bios.readByte(address);
+        if (address < ROM_1_START) {
+            return bios.readByte(address - BIOS_START);
+        } else if (address < VRAM_START){
+            return rom1.readByte(address - ROM_1_START);
+        } else if (address < EXT_RAM_START) {
+            return gpuVram.readByte(address - VRAM_START);
+        } else if (address < RAM_START){
+            return extRam.readByte(address - EXT_RAM_START);
+        } else if (address < SHADOW_RAM_START) {
+            return ram.readByte(address - RAM_START);
+        } else if (address < SPRITES_START) {
+            return ram.readByte(address - SHADOW_RAM_START);
+        } else if (address < DEAD_AREA_START) {
+            return sprites.readByte(address - SPRITES_START);
+        } else if (address < IO_AREA_START) {
+            throw new IllegalArgumentException("Cannot read from address " + address +
+                    ": memory in range " + Integer.toHexString(DEAD_AREA_START) +
+                    "-" + Integer.toHexString(IO_AREA_START - 1) +
+                    " is inaccessible");
+        } else if (address < ZRAM_START) {
+            return io.readByte(address - IO_AREA_START);
         } else {
-            return rom1.readByte(address - 0x4000);
+            return zram.readByte(address - ZRAM_START);
         }
     }
 }
--- src/test/java/com/kopieczek/gamble/memory/TestMemoryManagementUnit.java
+++ src/test/java/com/kopieczek/gamble/memory/TestMemoryManagementUnit.java
@@ -11,6 +11,7 @@ public class TestMemoryManagementUnit {
     private static final int VRAM_SIZE = 0x2000;
     private static final int EXTRAM_SIZE = 0x2000;
     private static final int RAM_SIZE = 0x2000;
+    private static final int SHADOW_RAM_SIZE = 0x1e00; // Shadow RAM shadows all but last 512 bytes of main RAM.
     private static final int SPRITES_SIZE = 0xA0;
     private static final int IO_SIZE = 0x80;
     private static final int ZRAM_SIZE = 0x80;
@@ -18,6 +19,15 @@ public class TestMemoryManagementUnit {
     private static final int BIOS_START = 0x0;
     private static final int ROM_0_START = 0x0;
     private static final int ROM_1_START = ROM_0_START + ROM_0_SIZE;
+    private static final int VRAM_START = ROM_1_START + ROM_1_SIZE;
+    private static final int EXTRAM_START = VRAM_START + VRAM_SIZE;
+    private static final int RAM_START = EXTRAM_START + EXTRAM_SIZE;
+    private static final int SHADOW_RAM_START = RAM_START + RAM_SIZE;
+    private static final int SPRITES_START = SHADOW_RAM_START + SHADOW_RAM_SIZE;
+    private static final int DEAD_AREA_START = SPRITES_START + SPRITES_SIZE;
+    private static final int DEAD_AREA_SIZE = 0x60;
+    private static final int IO_START = DEAD_AREA_START + DEAD_AREA_SIZE;
+    private static final int ZRAM_START = IO_START + IO_SIZE;
 
     @Test
     public void test_valid_memory_modules_accepted() {
@@ -184,7 +194,7 @@ public class TestMemoryManagementUnit {
                 new SimpleMemoryModule(ZRAM_SIZE)
         );
 
-        testMmuRead(mmu, bios, ROM_0_START);
+        testMmuRead(mmu, bios, BIOS_START);
     }
 
     @Test
@@ -205,10 +215,170 @@ public class TestMemoryManagementUnit {
         testMmuRead(mmu, rom1, ROM_1_START);
     }
 
-    private void testMmuRead(MemoryManagementUnit mmu, MemoryModule module, int addressOffset) {
+    @Test
+    public void test_read_from_gpu_vram() {
+        SimpleMemoryModule vram = new SimpleMemoryModule(VRAM_SIZE);
+        MemoryManagementUnit mmu = new MemoryManagementUnit(
+                new SimpleMemoryModule(BIOS_SIZE),
+                new SimpleMemoryModule(ROM_0_SIZE),
+                new SimpleMemoryModule(ROM_1_SIZE),
+                vram,
+                new SimpleMemoryModule(EXTRAM_SIZE),
+                new SimpleMemoryModule(RAM_SIZE),
+                new SimpleMemoryModule(SPRITES_SIZE),
+                new SimpleMemoryModule(IO_SIZE),
+                new SimpleMemoryModule(ZRAM_SIZE)
+        );
+
+        testMmuRead(mmu, vram, VRAM_START);
+    }
+
+    @Test
+    public void test_read_from_extram() {
+        SimpleMemoryModule extram = new SimpleMemoryModule(EXTRAM_SIZE);
+        MemoryManagementUnit mmu = new MemoryManagementUnit(
+                new SimpleMemoryModule(BIOS_SIZE),
+                new SimpleMemoryModule(ROM_0_SIZE),
+                new SimpleMemoryModule(ROM_1_SIZE),
+                new SimpleMemoryModule(VRAM_SIZE),
+                extram,
+                new SimpleMemoryModule(RAM_SIZE),
+                new SimpleMemoryModule(SPRITES_SIZE),
+                new SimpleMemoryModule(IO_SIZE),
+                new SimpleMemoryModule(ZRAM_SIZE)
+        );
+
+        testMmuRead(mmu, extram, EXTRAM_START);
+    }
+
+    @Test
+    public void test_read_from_ram() {
+        SimpleMemoryModule ram = new SimpleMemoryModule(RAM_SIZE);
+        MemoryManagementUnit mmu = new MemoryManagementUnit(
+                new SimpleMemoryModule(BIOS_SIZE),
+                new SimpleMemoryModule(ROM_0_SIZE),
+                new SimpleMemoryModule(ROM_1_SIZE),
+                new SimpleMemoryModule(VRAM_SIZE),
+                new SimpleMemoryModule(EXTRAM_SIZE),
+                ram,
+                new SimpleMemoryModule(SPRITES_SIZE),
+                new SimpleMemoryModule(IO_SIZE),
+                new SimpleMemoryModule(ZRAM_SIZE)
+        );
+
+        testMmuRead(mmu, ram, RAM_START);
+    }
+
+    @Test
+    public void test_read_from_shadow_ram() {
+        SimpleMemoryModule ram = new SimpleMemoryModule(RAM_SIZE);
+        MemoryManagementUnit mmu = new MemoryManagementUnit(
+                new SimpleMemoryModule(BIOS_SIZE),
+                new SimpleMemoryModule(ROM_0_SIZE),
+                new SimpleMemoryModule(ROM_1_SIZE),
+                new SimpleMemoryModule(VRAM_SIZE),
+                new SimpleMemoryModule(EXTRAM_SIZE),
+                ram,
+                new SimpleMemoryModule(SPRITES_SIZE),
+                new SimpleMemoryModule(IO_SIZE),
+                new SimpleMemoryModule(ZRAM_SIZE)
+        );
+
+        testMmuRead(mmu, ram, SHADOW_RAM_START, SHADOW_RAM_SIZE);
+    }
+
+    @Test
+    public void test_read_from_sprite_area() {
+        SimpleMemoryModule sprites = new SimpleMemoryModule(SPRITES_SIZE);
+        MemoryManagementUnit mmu = new MemoryManagementUnit(
+                new SimpleMemoryModule(BIOS_SIZE),
+                new SimpleMemoryModule(ROM_0_SIZE),
+                new SimpleMemoryModule(ROM_1_SIZE),
+                new SimpleMemoryModule(VRAM_SIZE),
+                new SimpleMemoryModule(EXTRAM_SIZE),
+                new SimpleMemoryModule(RAM_SIZE),
+                sprites,
+                new SimpleMemoryModule(IO_SIZE),
+                new SimpleMemoryModule(ZRAM_SIZE)
+        );
+
+        testMmuRead(mmu, sprites, SPRITES_START);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void test_read_from_unused_area_start() {
+        MemoryManagementUnit mmu = new MemoryManagementUnit(
+                new SimpleMemoryModule(BIOS_SIZE),
+                new SimpleMemoryModule(ROM_0_SIZE),
+                new SimpleMemoryModule(ROM_1_SIZE),
+                new SimpleMemoryModule(VRAM_SIZE),
+                new SimpleMemoryModule(EXTRAM_SIZE),
+                new SimpleMemoryModule(RAM_SIZE),
+                new SimpleMemoryModule(SPRITES_SIZE),
+                new SimpleMemoryModule(IO_SIZE),
+                new SimpleMemoryModule(ZRAM_SIZE)
+        );
+
+        mmu.readByte(DEAD_AREA_START);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void test_read_from_unused_area_end() {
+        MemoryManagementUnit mmu = new MemoryManagementUnit(
+                new SimpleMemoryModule(BIOS_SIZE),
+                new SimpleMemoryModule(ROM_0_SIZE),
+                new SimpleMemoryModule(ROM_1_SIZE),
+                new SimpleMemoryModule(VRAM_SIZE),
+                new SimpleMemoryModule(EXTRAM_SIZE),
+                new SimpleMemoryModule(RAM_SIZE),
+                new SimpleMemoryModule(SPRITES_SIZE),
+                new SimpleMemoryModule(IO_SIZE),
+                new SimpleMemoryModule(ZRAM_SIZE)
+        );
+
+        mmu.readByte(DEAD_AREA_START + DEAD_AREA_SIZE - 1);
+    }
+
+    @Test
+    public void test_read_from_io_area() {
+        SimpleMemoryModule io = new SimpleMemoryModule(IO_SIZE);
+        MemoryManagementUnit mmu = new MemoryManagementUnit(
+                new SimpleMemoryModule(BIOS_SIZE),
+                new SimpleMemoryModule(ROM_0_SIZE),
+                new SimpleMemoryModule(ROM_1_SIZE),
+                new SimpleMemoryModule(VRAM_SIZE),
+                new SimpleMemoryModule(EXTRAM_SIZE),
+                new SimpleMemoryModule(RAM_SIZE),
+                new SimpleMemoryModule(SPRITES_SIZE),
+                io,
+                new SimpleMemoryModule(ZRAM_SIZE)
+        );
+
+        testMmuRead(mmu, io, IO_START);
+    }
+
+    @Test
+    public void test_read_from_zram() {
+        SimpleMemoryModule zram = new SimpleMemoryModule(ZRAM_SIZE);
+        MemoryManagementUnit mmu = new MemoryManagementUnit(
+                new SimpleMemoryModule(BIOS_SIZE),
+                new SimpleMemoryModule(ROM_0_SIZE),
+                new SimpleMemoryModule(ROM_1_SIZE),
+                new SimpleMemoryModule(VRAM_SIZE),
+                new SimpleMemoryModule(EXTRAM_SIZE),
+                new SimpleMemoryModule(RAM_SIZE),
+                new SimpleMemoryModule(SPRITES_SIZE),
+                new SimpleMemoryModule(ZRAM_SIZE),
+                zram
+        );
+
+        testMmuRead(mmu, zram, ZRAM_START);
+    }
+
+    private void testMmuRead(MemoryManagementUnit mmu, MemoryModule module, int addressOffset, int maxSize) {
         int start = 0;
         int mid = module.getSizeInBytes() / 2;
-        int end = module.getSizeInBytes() - 1;
+        int end = maxSize - 1;
         int startVal = 0x01;
         int midVal = 0xa0;
         int endVal = 0xff;
@@ -218,5 +388,9 @@ public class TestMemoryManagementUnit {
         assertEquals(startVal, mmu.readByte(addressOffset + start));
         assertEquals(midVal, mmu.readByte(addressOffset + mid));
         assertEquals(endVal, mmu.readByte(addressOffset + end));
+    }
+
+    private void testMmuRead(MemoryManagementUnit mmu, MemoryModule module, int addressOffset) {
+        testMmuRead(mmu, module, addressOffset, module.getSizeInBytes());
     }
 }
