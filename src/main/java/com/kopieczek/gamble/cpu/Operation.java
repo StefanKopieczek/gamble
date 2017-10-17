57547a25c6a7ab30405bde4f6e82d880c7c015c4
Stefan Kopieczek <stefankopieczek@gmail.com>
2017-10-17 21:50:43 +0100

    CPU: Move cycle count out of Operation constructor
    Instead have the operation's 'action' function return the total number
    of cycles used.

--- src/main/java/com/kopieczek/gamble/cpu/Operation.java
+++ src/main/java/com/kopieczek/gamble/cpu/Operation.java
@@ -5,71 +5,71 @@ import java.util.Map;
 import java.util.function.Function;
 
 enum Operation {
-    NOP(0x00, 4, FlagHandler.none(), (cpu) -> null),
-    INC_A(0x3c, 4,
+    NOP(0x00, FlagHandler.none(), (cpu) -> 4),
+    INC_A(0x3c,
           new FlagHandler().setZeroFlagFrom(Register.A)
                            .setNibbleFlagFrom(Register.A)
                            .opFlag(FlagHandler.Strategy.RESET),
           (cpu) -> {
               cpu.increment(Register.A);
-              return null;
+              return 4;
           }
     ),
-    INC_B(0x04, 4,
+    INC_B(0x04,
           new FlagHandler().setZeroFlagFrom(Register.B)
                            .setNibbleFlagFrom(Register.B)
                            .opFlag(FlagHandler.Strategy.RESET),
           (cpu) -> {
               cpu.increment(Register.B);
-              return null;
+              return 4;
           }
     ),
-    INC_C(0x0c, 4,
+    INC_C(0x0c,
           new FlagHandler().setZeroFlagFrom(Register.C)
                            .setNibbleFlagFrom(Register.C)
                            .opFlag(FlagHandler.Strategy.RESET),
           (cpu) -> {
               cpu.increment(Register.C);
-              return null;
+              return 4;
           }
     ),
-    INC_D(0x14, 4,
+    INC_D(0x14,
           new FlagHandler().setZeroFlagFrom(Register.D)
                            .setNibbleFlagFrom(Register.D)
                            .opFlag(FlagHandler.Strategy.RESET),
           (cpu) -> {
               cpu.increment(Register.D);
-              return null;
+              return 4;
           }
     ),
-    INC_E(0x1c, 4,
+    INC_E(0x1c,
           new FlagHandler().setZeroFlagFrom(Register.E)
                            .setNibbleFlagFrom(Register.E)
                            .opFlag(FlagHandler.Strategy.RESET),
           (cpu) -> {
               cpu.increment(Register.E);
-              return null;
+              return 4;
           }
     ),
-    INC_H(0x24, 4,
+    INC_H(0x24,
           new FlagHandler().setZeroFlagFrom(Register.H)
                            .setNibbleFlagFrom(Register.H)
                            .opFlag(FlagHandler.Strategy.RESET),
           (cpu) -> {
               cpu.increment(Register.H);
-              return null;
+              return 4;
           }
     ),
-    INC_L(0x2c, 4,
+    INC_L(0x2c,
           new FlagHandler().setZeroFlagFrom(Register.L)
                            .setNibbleFlagFrom(Register.L)
                            .opFlag(FlagHandler.Strategy.RESET),
           (cpu) -> {
               cpu.increment(Register.L);
-              return null;
+              return 4;
           }
     ),
-    INC_HL(0x34, 12,
+    INC_HL(0x34,
             new FlagHandler().setZeroFlagFrom(Register.H)
                     .setNibbleFlagFrom(Register.L)
                     .opFlag(FlagHandler.Strategy.RESET),
@@ -78,7 +78,7 @@ enum Operation {
                 if (cpu.readByte(Register.L) == 0) {
                     cpu.increment(Register.H);
                 }
-                return null;
+                return 12;
             }
     );
 
@@ -91,26 +91,22 @@ enum Operation {
 
     private int opcode;
     private FlagHandler flagHandler;
-    int cycleCount;
-    Function<Cpu, Void> action;
+    Function<Cpu, Integer> action;
 
-    Operation(int opcode, int cycleCount, FlagHandler flagHandler, Function<Cpu, Void> action) {
+    Operation(int opcode, FlagHandler flagHandler, Function<Cpu, Integer> action) {
         this.opcode = opcode;
         this.flagHandler = flagHandler;
-        this.cycleCount = cycleCount;
         this.action = action;
     }
 
     public void execute(Cpu cpu) {
-        cpu.cycles += cycleCount;
-
         Register nibbleRegister = flagHandler.getNibbleFlagRegister();
         Integer nibbleRegisterBefore = null;
         if (nibbleRegister != null) {
             nibbleRegisterBefore = cpu.readByte(nibbleRegister);
         }
 
-        action.apply(cpu);
+        cpu.cycles += action.apply(cpu);
 
         if (nibbleRegister != null) {
             int nibbleRegisterAfter = cpu.readByte(nibbleRegister);
