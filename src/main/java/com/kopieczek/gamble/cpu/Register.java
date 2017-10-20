dd32a59c8c6ff82c107c72e7eb310428b64ed2b4
Stefan Kopieczek <stefankopieczek@gmail.com>
2017-10-20 22:33:55 +0100

    CPU: Tidy up access to HL register

--- src/main/java/com/kopieczek/gamble/cpu/Register.java
+++ src/main/java/com/kopieczek/gamble/cpu/Register.java
@@ -1,5 +1,9 @@
 package com.kopieczek.gamble.cpu;
 
+import com.kopieczek.gamble.memory.IndirectAddress;
+
 public enum Register {
     A, B, C, D, E, H, L;
+
+    public static IndirectAddress HL = IndirectAddress.from(H, L);
 }
