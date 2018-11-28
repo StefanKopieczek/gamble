fc3ac0681b8b29b45e7bf09a07bfab5a776a4c96
Stefan Kopieczek <stefankopieczek@gmail.com>
2018-11-28 22:18:11 +0000

    Tidy up TestOamModule
    Create a test helper and do some refactoring.

--- src/test/java/com/kopieczek/gamble/hardware/memory/TestOamModule.java
+++ src/test/java/com/kopieczek/gamble/hardware/memory/TestOamModule.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
@@ -14,28 +14,18 @@ public class TestOamModule {
     @Test
     public void test_writes_fire_data_modified_events_for_correct_sprites() {
         final AtomicInteger lastModified = new AtomicInteger(-1);
-
-        SpriteChangeListener listener = new SpriteChangeListener() {
-            @Override
-            public void onSpriteDataModified(int spriteIndex) {
-                lastModified.set(spriteIndex);
-            }
-
-            @Override
-            public void onSpritePatternModified(int patternIndex) {
-                // Do nothing
-            }
-        };
-
-        OamModule oam = new OamModule();
-        oam.register(listener);
-
-        for (int address = 0x00; address < 0xa0; address++) {
-            oam.setByte(address, 0xff);
-            int expectedSprite = address / 4;
-            assertEquals(String.format("Expected write to 0x%02x to fire change on sprite %d", address, expectedSprite),
-                    expectedSprite, lastModified.get());
-        }
+        doSpriteDataChangeTest(
+            oam -> {
+                for (int address = 0x00; address < 0xa0; address++) {
+                    oam.setByte(address, 0xff);
+                    int expectedSprite = address / 4;
+                    assertEquals(String.format("Expected write to 0x%02x to fire change on sprite %d",
+                                               address, expectedSprite),
+                            expectedSprite, lastModified.get());
+                }
+            },
+            lastModified::set
+        );
     }
 
     @Test
@@ -60,56 +50,43 @@ public class TestOamModule {
     @Test
     public void test_all_writes_fire_pattern_modified_events() {
         AtomicInteger numEvents = new AtomicInteger(0);
-        OamModule oam = new OamModule();
-        oam.setByte(0x01, 0x01);
-
-        SpriteChangeListener listener = new SpriteChangeListener() {
-            @Override
-            public void onSpriteDataModified(int spriteIndex) {
-                numEvents.incrementAndGet();
-            }
-
-            @Override
-            public void onSpritePatternModified(int patternIndex) {
-                // Do nothing
-            }
-        };
-
-        oam.register(listener);
-        IntStream.range(0, 256).forEach(val -> oam.setByte(0x01, val));
+        doSpriteDataChangeTest(
+            oam -> {
+                for (int val = 0xff; val >= 0; val--) {
+                    oam.setByte(0x00, val);
+                }
+            },
+            changedSprite -> numEvents.incrementAndGet()
+        );
         assertEquals(256, numEvents.get());
     }
 
     @Test
     public void test_initial_zero_write_does_not_fire_data_modified() {
-        SpriteChangeListener listener = new SpriteChangeListener() {
-            @Override
-            public void onSpriteDataModified(int spriteIndex) {
-                fail();
-            }
-
-            @Override
-            public void onSpritePatternModified(int patternIndex) {
-            }
-        };
-
-        OamModule oam = new OamModule();
-        oam.register(listener);
-        oam.setByte(0x00, 0x00);
+        doSpriteDataChangeTest(
+            oam -> oam.setByte(0x00, 0x00),
+            changedSprite -> fail()
+        );
     }
 
     @Test
     public void test_repeated_writes_only_fire_data_modified_once() {
-        SpriteChangeListener listener = new SpriteChangeListener() {
-            private boolean alreadyFired = false;
+        AtomicBoolean alreadyFired = new AtomicBoolean(false);
+        doSpriteDataChangeTest(
+            oam -> IntStream.range(0, 1000).forEach(x -> oam.setByte(0x00, 0xff)),
+            changedSprite ->  {
+                if (alreadyFired.getAndSet(true)) {
+                    fail();
+                }
+            }
+        );
+    }
 
+    private void doSpriteDataChangeTest(Consumer<OamModule> test, Consumer<Integer> onDataChange) {
+        SpriteChangeListener listener = new SpriteChangeListener() {
             @Override
             public void onSpriteDataModified(int spriteIndex) {
-                if (alreadyFired) {
-                    fail();
-                } else {
-                    alreadyFired = true;
-                }
+                onDataChange.accept(spriteIndex);
             }
 
             @Override
@@ -120,6 +97,6 @@ public class TestOamModule {
 
         OamModule oam = new OamModule();
         oam.register(listener);
-        IntStream.range(0, 1000).forEach(x -> oam.setByte(0x00, 0xff));
+        test.accept(oam);
     }
 }
