794731e4b3f88df7f9dacde114b3bc472bf8d482
Stefan Kopieczek <stefankopieczek@gmail.com>
2018-12-02 15:31:22 +0000

    Implement sprite invalidation and reload logic

--- src/main/java/com/kopieczek/gamble/hardware/graphics/SpriteMap.java
+++ src/main/java/com/kopieczek/gamble/hardware/graphics/SpriteMap.java
@@ -1,12 +1,15 @@
 package com.kopieczek.gamble.hardware.graphics;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Multimap;
 import com.kopieczek.gamble.hardware.memory.Io;
 import com.kopieczek.gamble.hardware.memory.Oam;
 import com.kopieczek.gamble.hardware.memory.SpriteChangeListener;
 import com.kopieczek.gamble.hardware.memory.Vram;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class SpriteMap implements SpriteChangeListener {
@@ -16,6 +19,13 @@ class SpriteMap implements SpriteChangeListener {
     private final List<SpriteAttributes> allAttributes = new ArrayList<>();
     private final List<SpritePattern> patterns = new ArrayList<>();
     private final List<Sprite> sprites = new ArrayList<>();
+    private final Set<Integer> dirtyAttributes = new HashSet<>();
+    private final Set<Integer> dirtyPatterns = new HashSet<>();
+    private final Set<Integer> dirtySprites = new HashSet<>();
+
+    // NB: The following fields are only guaranteed to be up to date when dirtyAttributes is empty.
+    private final Multimap<Integer, Integer> patternToSpriteMap = ArrayListMultimap.create();
+    private final Multimap<Integer, Integer> rowToSpriteMap = ArrayListMultimap.create();
 
     private boolean useTallSprites = false;
 
@@ -34,33 +44,96 @@ class SpriteMap implements SpriteChangeListener {
 
     private void reloadAll() {
         reloadAllAttributes();
-        reloadPatterns();
+        reloadAllPatterns();
         loadSpriteHeight();
-        rebuildSprites();
+        rebuildAllSprites();
     }
 
     private void reloadAllAttributes() {
         allAttributes.clear();
+        dirtyAttributes.clear();
+        patternToSpriteMap.clear();
         IntStream.range(0, Oam.TOTAL_ATTRIBUTES).forEach(idx -> {
             int[] data = oam.getAttributeBytes(idx);
-            allAttributes.add(SpriteAttributes.parse(data));
+            SpriteAttributes attrs = SpriteAttributes.parse(data);
+            allAttributes.add(attrs);
+            getPatternsUsed(attrs).forEach(pattern -> patternToSpriteMap.put(pattern, idx));
+            getRowsUsed(attrs).forEach(row -> rowToSpriteMap.put(row, idx));
         });
     }
 
-    private void reloadPatterns() {
-        patterns.clear();
-        IntStream.range(0, Vram.TOTAL_SPRITE_PATTERNS).forEach(idx -> {
-            int[] compressedPattern = vram.getPatternBytes(idx);
-            patterns.add(SpritePattern.fromCompressed(compressedPattern));
+    private void reloadDirtyAttributes() {
+        reloadAttributes(dirtyAttributes);
+    }
+
+    private void reloadAttributes(Collection<Integer> attrsToReload) {
+        attrsToReload.forEach(spriteIndex -> {
+            // Remove existing attribute based-mappings for this sprite.
+            SpriteAttributes oldAttrs = allAttributes.get(spriteIndex);
+            getPatternsUsed(oldAttrs).forEach(pattern -> patternToSpriteMap.remove(pattern, spriteIndex));
+            getRowsUsed(oldAttrs).forEach(row -> rowToSpriteMap.remove(row, spriteIndex));
+
+            // Reload attributes
+            int[] data = oam.getAttributeBytes(spriteIndex);
+            SpriteAttributes newAttrs = SpriteAttributes.parse(data);
+            allAttributes.set(spriteIndex, newAttrs);
+
+            // Add new attribute-based mappings for the reloaded value.
+            getPatternsUsed(newAttrs).forEach(pattern -> patternToSpriteMap.put(pattern, spriteIndex));
+            getRowsUsed(newAttrs).forEach(row -> rowToSpriteMap.put(row, spriteIndex));
         });
+        dirtyAttributes.removeAll(attrsToReload);
+    }
+
+    private void reloadAllPatterns() {
+        assert(dirtyAttributes.size() == 0);
+        patterns.clear();
+        dirtyPatterns.clear();
+        IntStream.range(0, Vram.TOTAL_SPRITE_PATTERNS).forEach(patternIdx -> {
+            int[] compressedPattern = vram.getPatternBytes(patternIdx);
+            patterns.add(SpritePattern.fromCompressed(compressedPattern));
+            dirtySprites.addAll(patternToSpriteMap.get(patternIdx));
+        });
+    }
+
+    private void reloadPatterns(Collection<Integer> patternsToReload) {
+        assert(dirtyAttributes.size() == 0);
+        patternsToReload.forEach(patternIdx -> {
+            int[] compressedPattern = vram.getPatternBytes(patternIdx);
+            patterns.set(patternIdx, SpritePattern.fromCompressed(compressedPattern));
+            dirtySprites.addAll(patternToSpriteMap.get(patternIdx));
+        });
+    }
+
+    private void ensureSpritesAreClean(Collection<Integer> spriteIndexes) {
+        reloadDirtyAttributes();
+
+        Set<Integer> patternsToReload = spriteIndexes.stream()
+                .map(allAttributes::get)
+                .flatMap(sprite -> getPatternsUsed(sprite).stream())
+                .filter(dirtyPatterns::contains)
+                .collect(Collectors.toSet());
+        reloadPatterns(patternsToReload);
+
+        List<Integer> spritesToRebuild = spriteIndexes.stream().filter(dirtySprites::contains).collect(Collectors.toList());
+        rebuildSprites(spritesToRebuild);
+    }
+
+    private void ensureRowIsClean(int rowIdx) {
+        // We have to validate attributes first, or we can't reliably tell which sprites are on the row.
+        reloadDirtyAttributes();
+
+        Collection<Integer> spritesOnRow = rowToSpriteMap.get(rowIdx);
+        ensureSpritesAreClean(spritesOnRow);
     }
 
     private void loadSpriteHeight() {
         useTallSprites = (io.getSpriteHeight() == 16);
     }
 
-    private void rebuildSprites() {
+    private void rebuildAllSprites() {
         sprites.clear();
+        dirtySprites.clear();
         allAttributes.forEach(attributes -> {
             if (useTallSprites) {
                 sprites.add(buildTallSprite(attributes));
@@ -68,6 +141,20 @@ class SpriteMap implements SpriteChangeListener {
                 sprites.add(buildShortSprite(attributes));
             }
         });
+    }
+
+    private void rebuildSprites(Collection<Integer> spritesToRebuild) {
+        spritesToRebuild.forEach(spriteIdx -> {
+            SpriteAttributes attributes = allAttributes.get(spriteIdx);
+            Sprite sprite;
+            if (useTallSprites) {
+                sprite = buildTallSprite(attributes);
+            } else {
+                sprite = buildShortSprite(attributes);
+            }
+            sprites.set(spriteIdx, sprite);
+        });
+        dirtySprites.removeAll(spritesToRebuild);
     }
 
     private Sprite buildShortSprite(SpriteAttributes attributes) {
@@ -83,18 +170,43 @@ class SpriteMap implements SpriteChangeListener {
         return new Sprite(attributes, pattern1, pattern2);
     }
 
+    private Set<Integer> getPatternsUsed(SpriteAttributes attrs) {
+        int mainPattern = attrs.getPatternIndex();
+        if (useTallSprites) {
+            int twinnedPattern = mainPattern ^ 0xfe;  // Flip final bit
+            return ImmutableSet.of(mainPattern, twinnedPattern);
+        } else {
+            return ImmutableSet.of(mainPattern);
+        }
+    }
+
+    private Set<Integer> getRowsUsed(SpriteAttributes attrs) {
+        int numRows = useTallSprites ? 16 : 8;
+        int minRow = attrs.getY();
+        int maxRow = minRow + numRows;
+        return IntStream.range(minRow, maxRow).boxed().collect(Collectors.toSet());
+    }
+
     @Override
     public void onSpriteAttributesModified(int spriteIndex) {
-
+        dirtyAttributes.add(spriteIndex);
+        dirtySprites.add(spriteIndex);
     }
 
     @Override
     public void onSpritePatternModified(int patternIndex) {
-
+        dirtyPatterns.add(patternIndex);
+        // We can't reliably mark the associated sprites dirty here, since there may be outstanding dirty attributes,
+        // in which case it's not possible to determine which sprites are using which patterns.
+        // Instead we enforce the following:
+        //   1. When ensuring sprites are clean, they must explicitly check to see if their pattern(s) are dirty.
+        //   2. Reloading patterns may only occur when all attributes are clean.
+        //   3. When a pattern is reloaded, we must immediately mark all sprites using the pattern as dirty.
+        //      (We can determine the set of affected sprites reliably since by 2. the attributes are all clean)
     }
 
     @Override
     public void onSpriteHeightChanged(boolean areTallSpritesEnabled) {
-
+        IntStream.range(0, Oam.TOTAL_ATTRIBUTES).forEach(dirtyAttributes::add);
     }
 }
