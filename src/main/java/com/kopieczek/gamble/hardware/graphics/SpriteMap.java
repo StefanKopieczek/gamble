8808c2529e07589447f3dd2c4dc55e4b6f79477f
Stefan Kopieczek <stefankopieczek@gmail.com>
2018-12-02 13:48:45 +0000

    Hook up SpriteMap listeners

--- src/main/java/com/kopieczek/gamble/hardware/graphics/SpriteMap.java
+++ src/main/java/com/kopieczek/gamble/hardware/graphics/SpriteMap.java
@@ -2,14 +2,33 @@ package com.kopieczek.gamble.hardware.graphics;
 
 import com.kopieczek.gamble.hardware.memory.Io;
 import com.kopieczek.gamble.hardware.memory.Oam;
+import com.kopieczek.gamble.hardware.memory.SpriteChangeListener;
 import com.kopieczek.gamble.hardware.memory.Vram;
 
-public class SpriteMap {
+class SpriteMap implements SpriteChangeListener {
     private final Oam oam;
     private final Vram vram;
 
     SpriteMap(Io io, Oam oam, Vram vram) {
         this.oam = oam;
         this.vram = vram;
+        io.register(this);
+        oam.register(this);
+        vram.register(this);
+    }
+
+    @Override
+    public void onSpriteAttributesModified(int spriteIndex) {
+
+    }
+
+    @Override
+    public void onSpritePatternModified(int patternIndex) {
+
+    }
+
+    @Override
+    public void onSpriteHeightChanged(boolean areTallSpritesEnabled) {
+
     }
 }
