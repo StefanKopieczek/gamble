c0d0fecdb64cb11a0dab75f0f6a92193d1fa3feb
Stefan Kopieczek <stefankopieczek@gmail.com>
2018-12-27 23:52:24 +0000

    Add all cartridge types to loader
    Still only support MBC0 and MBC1, but throw better errors when we don't
    support a given type.

--- src/main/java/com/kopieczek/gamble/hardware/memory/cartridge/CartridgeLoader.java
+++ src/main/java/com/kopieczek/gamble/hardware/memory/cartridge/CartridgeLoader.java
@@ -1,6 +1,5 @@
 package com.kopieczek.gamble.hardware.memory.cartridge;
 
-import com.kopieczek.gamble.hardware.memory.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -23,15 +22,74 @@ public class CartridgeLoader {
 
     public static Cartridge loadFrom(int[] data) {
         log.debug("Loading ROM from array of size {}", data.length);
-        int mbcType = data[MBC_TYPE_ADDR];
-        switch (mbcType) {
-            case 0:
+        int cartridgeType = data[MBC_TYPE_ADDR];
+        switch (cartridgeType) {
+            case 0: // ROM only
                 return new MbcType0Cartridge(data);
-            case 1:
+            case 1: // MBC type 1
+            case 2: // + RAM
+            case 3: // + RAM and battery
                 return new MbcType1Cartridge(data);
+            // ------- Type 4 does not exist
+            case 5: // MBC type 2
+            case 6: // + RAM and battery
+                unsupportedCartridge("MBC type 2");
+            // ------- Type 7 does not exist
+            case 8: // ROM + RAM
+            case 9: // + battery
+                return new MbcType0Cartridge(data);
+            // ------- Type 10 does not exist
+            case 11: // MMM01
+            case 12: // + RAM
+            case 13: // + RAM and battery
+                unsupportedCartridge("MM01");
+            // ------- Type 14 does not exist
+            case 15: // MBC type 3 + timer + battery
+            case 16: // MBC type 3 + RAM + timer + battery
+            case 17: // MBC type 3
+            case 18: // MBC type 3 + RAM
+            case 19: // MBC type 3 + RAM + battery
+                unsupportedCartridge("MBC type 3");
+            // ------- Types 20-24 do not exist
+            case 25: // MBC type 5
+            case 26: // + RAM
+            case 27: // + RAM + battery
+            case 28: // + rumble
+            case 29: // + RAM + rumble
+            case 30: // + RAM + battery + rumble
+                unsupportedCartridge("MBC type 5");
+            // ------- Type 31 does not exist
+            case 32: // MBC type 6 + RAM + battery
+                unsupportedCartridge("MBC type 6");
+            // ------- Type 33 does not exist
+            case 34: // MBC type 7 + RAM + battery + accelerometer
+                unsupportedCartridge("MBC type 7");
+            // ------- Types 35-251 do not exist
+            case 252: // GB Camera
+                unsupportedCartridge("Gameboy Camera");
+            case 253: // TAMA5
+                unsupportedCartridge("Bandai TAMA5");
+            case 254: // HuC3
+                unsupportedCartridge("HuC3");
+            case 255: // HuC1 + RAM + Battery
+                unsupportedCartridge("HuC1");
             default:
-                throw new IllegalArgumentException("Unknown MBC type " + mbcType);
+                invalidCartridgeType(cartridgeType);
         }
+
+        return null;
+    }
+
+    private static void invalidCartridgeType(int cartridgeType) {
+        throw new IllegalArgumentException("Cartridge uses invalid cartridge type " + cartridgeType + ", which does not exist");
+    }
+
+    private static void unsupportedCartridge(String cartridgeType) {
+        throw new UnsupportedOperationException("Cartridge uses " + cartridgeType + ", which is currently unsupported");
+    }
+
+    public static int getMbcTypeAddr() {
+        return MBC_TYPE_ADDR;
     }
 
     private static int[] mapToUnsigned(byte[] data) {
