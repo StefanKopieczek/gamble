6656f3494aaa4c46799204cbad475f6b377fc64c
Stefan Kopieczek <stefankopieczek@gmail.com>
2019-01-10 21:41:13 +0000

    Performance optimization to AbstractChannel
    AbstractChannel is responsible, among other things, for uplifting mono
    samples from the various channels into Stereo output. This means it
    creates a lot of width-two arrays. Profiling hints that this array
    creation might have been taking up a lot of time, so as an optimization
    we now use a large lookup table to map samples and audio modes to the
    corresponding (precalculated, unchanging) stereo output arrays.

--- src/main/java/com/kopieczek/gamble/hardware/audio/AbstractChannel.java
+++ src/main/java/com/kopieczek/gamble/hardware/audio/AbstractChannel.java
@@ -7,6 +7,10 @@ public abstract class AbstractChannel implements Channel, MasterAudioListener {
     private boolean isAudioOutputEnabled = false;
     private AudioOutputMode audioOutputMode = AudioOutputMode.NONE;
 
+    // Performance optimization so we don't have to create new stereo arrays for each sample on each tick.
+    // Instead, create a table that takes audio mode and sample, and returns the corresponding stereo array.
+    private final short[][][] sampleLookupTable = buildSampleLookupTable();
+
     public AbstractChannel(Io io) {
         this.io = io;
         io.register(this);
@@ -16,14 +20,7 @@ public abstract class AbstractChannel implements Channel, MasterAudioListener {
     public final short[] tick() {
         if (isAudioOutputEnabled) {
             short sample = getSample();
-            switch (audioOutputMode) {
-                case STEREO:
-                    return new short[] {sample, sample};
-                case LEFT_ONLY:
-                    return new short[] {sample, 0x00};
-                case RIGHT_ONLY:
-                    return new short[] {0x00, sample};
-            }
+            return sampleLookupTable[audioOutputMode.ordinal()][sample];
         }
 
         return new short[] {0x00, 0x00};
@@ -36,6 +33,36 @@ public abstract class AbstractChannel implements Channel, MasterAudioListener {
 
     protected void updateOutputMode(AudioOutputMode newOutputMode) {
         audioOutputMode = newOutputMode;
+    }
+
+    private static short[][][] buildSampleLookupTable() {
+        short[][][] table = new short[AudioOutputMode.values().length][][];
+        for (int modeInt = 0; modeInt < AudioOutputMode.values().length; modeInt++) {
+            table[modeInt] = new short[Short.MAX_VALUE][];
+            AudioOutputMode mode = AudioOutputMode.values()[modeInt];
+            for (short sample = 0; sample < Short.MAX_VALUE; sample++) {
+                short[] stereo;
+                switch (mode) {
+                    case NONE:
+                        stereo = new short[] {0x00, 0x00};
+                        break;
+                    case LEFT_ONLY:
+                        stereo = new short[] {sample, 0x00};
+                        break;
+                    case RIGHT_ONLY:
+                        stereo = new short[] {0x00, sample};
+                        break;
+                    case STEREO:
+                        stereo = new short[] {sample, sample};
+                        break;
+                    default:
+                        throw new IllegalStateException("Unknown audio output mode " + mode);
+                }
+                table[modeInt][sample] = stereo;
+            }
+        }
+
+        return table;
     }
 
     protected abstract short getSample();
